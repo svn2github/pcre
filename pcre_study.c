b17a62a5663fbb1eda289040153ed59188551e15
ph10 <ph10@2f5784b3-3f2a-0410-8824-cb99058d5e15>
2008-07-07 15:44:24 +0000

    Fix SKIPZERO bug in pcre_study().
    git-svn-id: svn://vcs.exim.org/pcre/code/trunk@353 2f5784b3-3f2a-0410-8824-cb99058d5e15

--- pcre_study.c
+++ pcre_study.c
@@ -220,6 +220,7 @@ do
       /* SKIPZERO skips the bracket. */
 
       case OP_SKIPZERO:
+      tcode++; 
       do tcode += GET(tcode,1); while (*tcode == OP_ALT);
       tcode += 1 + LINK_SIZE;
       break;
