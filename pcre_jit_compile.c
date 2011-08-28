2c4636957ccfe419a5825b3724af37fdc111b491
ph10 <ph10@2f5784b3-3f2a-0410-8824-cb99058d5e15>
2011-08-28 15:23:03 +0000

    Documentation for JIT support.
    git-svn-id: svn://vcs.exim.org/pcre/code/trunk@678 2f5784b3-3f2a-0410-8824-cb99058d5e15

--- pcre_jit_compile.c
+++ pcre_jit_compile.c
@@ -6388,7 +6388,9 @@ PCRE_EXP_DECL void
 pcre_assign_jit_stack(pcre_extra *extra, pcre_jit_callback callback, void *userdata)
 {
 executable_function *function;
-if ((extra->flags & PCRE_EXTRA_EXECUTABLE_JIT) != 0 && extra->executable_jit != NULL)
+if (extra != NULL &&
+    (extra->flags & PCRE_EXTRA_EXECUTABLE_JIT) != 0 && 
+    extra->executable_jit != NULL)
   {
   function = (executable_function*)extra->executable_jit;
   function->callback = callback;
