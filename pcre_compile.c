77b62a421481e0547788d4c0dc7539ac7f41d85b
ph10 <ph10@2f5784b3-3f2a-0410-8824-cb99058d5e15>
2011-12-18 10:03:38 +0000

    Renamed isnumber in pcre_compile to avoid a clash with ctype.h in Macs, and fixed a bug in fixed-length calculation for lookbehinds that would show up only in quite long subpatterns.
    git-svn-id: svn://vcs.exim.org/pcre/code/trunk@807 2f5784b3-3f2a-0410-8824-cb99058d5e15

--- pcre_compile.c
+++ pcre_compile.c
@@ -1655,7 +1655,8 @@ for (;;)
     case OP_ASSERTBACK:
     case OP_ASSERTBACK_NOT:
     do cc += GET(cc, 1); while (*cc == OP_ALT);
-    /* Fall through */
+    cc += _pcre_OP_lengths[*cc];
+    break;
 
     /* Skip over things that don't match chars */
 
@@ -6450,10 +6451,10 @@ for (;; ptr++)
 
         if (ptr[1] != CHAR_PLUS && ptr[1] != CHAR_MINUS)
           {
-          BOOL isnumber = TRUE;
+          BOOL is_a_number = TRUE;
           for (p = ptr + 1; *p != 0 && *p != terminator; p++)
             {
-            if ((cd->ctypes[*p] & ctype_digit) == 0) isnumber = FALSE;
+            if ((cd->ctypes[*p] & ctype_digit) == 0) is_a_number = FALSE;
             if ((cd->ctypes[*p] & ctype_word) == 0) break;
             }
           if (*p != terminator)
@@ -6461,7 +6462,7 @@ for (;; ptr++)
             *errorcodeptr = ERR57;
             break;
             }
-          if (isnumber)
+          if (is_a_number)
             {
             ptr++;
             goto HANDLE_NUMERICAL_RECURSION;
